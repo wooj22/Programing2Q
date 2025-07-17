c310597565137bf18e3259e10fecf70fac471af6
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-07-17 12:41:37 +0900

    stl, algorithm, functor, lamda

--- Programing2Q_Project/Programing2Q_Project/test.cpp
+++ Programing2Q_Project/Programing2Q_Project/test.cpp
@@ -1,1 +1,51 @@
-﻿
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+// 함수 타입 : 함수 원형에서 함수 이름이랑 매개변수 이름 빼면 됨
+// void(int) type
+void print(int n)
+{
+	cout << n << " ";
+}
+
+struct Print {
+	// void(int) type
+	void operator()(int n) { cout << n << " "; }
+};
+
+
+int main()
+{
+	int arr[] = { 2,4,1,2,4 };
+	vector<int> v(arr, arr+5);
+
+
+	// sort
+	sort(v.begin(), v.end());
+
+	// print
+	for (auto& it : v)
+	{
+		cout << it << " ";
+	}
+
+	// for each
+	for_each(v.begin(), v.end(), print);
+	for_each(v.begin(), v.end(), Print());
+
+	// 함수 객체
+	function<void(int)> p = print;
+	function<void(int)> ps = Print();
+
+	// 람다
+	[]() {cout << "heollo" << endl; };
+	[](int x) {cout << x << endl; };
+	[](int x) {return x % 2; };
+	[](int x, int y) {return x + y; };
+
+	return 0;
+}
